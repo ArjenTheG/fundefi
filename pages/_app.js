6565d19ae26bdd99e0989bd40e00bcf3121ee349
mdzakirhossen23 <158450965+mdzakirhossen23@users.noreply.github.com>
2024-05-31 23:07:33 +0600

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { UtilsProvider } from '../contexts/UtilsContext';
 import { UniquePolkadotProvider } from '../contexts/UniquePolkadotContext';
+import { EnvironmentProvider } from '../contexts/EnvironmentContext';
 import Header from '../components/layout/Header';
 import '../public/output.css';
 import '../public/theme.css';
@@ -11,9 +12,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <UtilsProvider>
       <UniquePolkadotProvider>
+      <EnvironmentProvider>
             <Header />
             <Component {...pageProps} />
             <ToastContainer hideProgressBar={false} position="top-right" autoClose={3000} newestOnTop={false} closeOnClick rtl={false} draggable pauseOnHover theme="light" />
+        </EnvironmentProvider>
       </UniquePolkadotProvider>
     </UtilsProvider>
   );
